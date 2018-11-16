4be3b4f0ca8bfeaedffa569c1d3d8a5f3781d233
Michael DeHaan <michael@michaeldehaan.net>
2018-11-16 08:05:34 -0500

    file docs WIP

--- module_docs/file.py
+++ module_docs/file.py
@@ -1,83 +1,111 @@
+# MODULE:          file
+# CATEGORY:        general
+# PROVIDERS:       file
+# RELATED_MODULES: directory
+# FYI:             See the online documentation at http://opsmop.io/modules.html for the full parameter list
 #
-# Module:     File
-# Type:       file
-# Provider:   file
-#
-# //TODO: (auto-load the parameters by instantiating the type, sort and pull off the help texts)
-#
-# Description:
+# DESCRIPTION
 # 
 # The File module handles all major types of file operations in OpsMop.
-#
 
 from opsmop.core.easy import *
 
-# Example: Template
+# --------------------------------------------------------------------------------------
+# EXAMPLE: Template
+# SEE_FILE: templates/foo.txt.j2
 #
 # Templating a file from a jinja2 template
-#
-# Embed: templates/foo.txt.j2
 
-class Jinja2Example(Role):
+class Jinja2TemplateExample(Role):
 
     def set_variables(self):
         return dict(a=1, b=5150, c="badwolf")
 
     def set_resources(self):
         return Resources(
+            # for template language and variable scoping information, please consult the language docs
             File(name="/tmp/opsmop-demo/foo1.txt", from_template="templates/foo.txt.j2"),
-            Shell("cat /tmp/opsmop-demo/foo1.txt"
+            Shell("cat /tmp/opsmop-demo/foo1.txt")
         )
 
-# Example: Copy
+
+# --------------------------------------------------------------------------------------
+# EXAMPLE: Copy
+#
+# DESCRIPTION
 #
 # Copying a file with owner, permission, and mode
 
 class CopyExample(Role):
 
-    def resources(self):
+    def set_resources(self):
         return Resources(
-             File(name="/tmp/opsmop-demo/foo2.txt", from_file="files/foo.txt", owner=root, mode=0x755),
+             # owner/group/mode can be used with any of these forms, just showing one example here
+             File(name="/tmp/opsmop-demo/foo2.txt", from_file="files/foo.txt", owner='root', mode=0x755),
              Shell("cat /tmp/opsmop-demo/foo2.txt")
         )    
 
-# Example: Copy From String
+# --------------------------------------------------------------------------------------
+# EXAMPLE: Copy From String
+#
+# DESCRIPTION
 #
 # For very small files, this is also possible
 
 class ContentExample(Role):
 
-    def resources(self):
+    def set_resources(self):
         return Resources(
-             File(name="/tmp/opsmop-demo/foo3.txt", content="Happy Birthday"),
-             Shell("cat /tmp/opsmop-demo/foo3.txt")
+             File(name="/tmp/opsmop-demo/foo3.txt", from_content="Happy Birthday"),
+             Shell("cat /tmp/opsmop-demo/foo3.txt"),
+             File(name="/tmp/opsmop-demo/foo4.txt", from_content=T("Template test! a={{ a}}")),
+             Shell("cat /tmp/opsmop-demo/foo4.txt")
         )
 
-# Prepare: This is a common role that is used to setup for the examples below
+# --------------------------------------------------------------------------------------
+# EXAMPLE: Deleting a File
+#
+# DESCRIPTION
+#
+# Ensure that a file does not exist
+
+class AbsentExample(Role):
+
+    def set_resources(self):
+        return Resources(
+             File(name="/tmp/opsmop-demo/foo4.txt", absent=True),
+             Shell("ls /tmp/opsmop-demo/foo4.txt", ignore_errors=True),
+             # the file is already deleted so this next step is a no-op
+             File(name="/tmp/opsmop-demo/foo4.txt", absent=True),
+        )
+
+
+# --------------------------------------------------------------------------------------
+# SETUP: a helper role that sets up for this demo
 
 class CommonSetup(Role):
 
-    def resources(self):
-        return Directory(name="/tmp/opsmop-demo/")
+    def set_resources(self):
+        return Roles(
+              Directory(name="/tmp/opsmop-demo/")
+        )
 
-# Policy: This runs the Demo
+# --------------------------------------------------------------------------------------
+# POLICY: loads all of the above roles
 
 class Demo(Policy):
 
-    def roles(self):
+    def set_roles(self):
        return Roles(
            CommonSetup(),
            Jinja2TemplateExample(d=4, e=5, f=6),
            CopyExample(),
-           ContentExample()
+           ContentExample(),
+           AbsentExample()
        )
 
 def main():
     return [ Demo() ]
-
-
-
-
 
 
 
