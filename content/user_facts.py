f4ef93118bf471eedb7e90f08ce9ce624d782ac6
Michael DeHaan <michael@michaeldehaan.net>
2018-11-20 18:35:48 -0500

    Update user_facts.py

--- content/user_facts.py
+++ content/user_facts.py
@@ -1,10 +1,6 @@
 # this is a demo of UserFacts which uses files baked into 
 # /etc/opsmop/facts.d to implement feature flags
 
-# for simplicity, this demo ALSO sets up those feature flags
-# in /etc/opsmop/facts.d, which isn't a real world kind
-# of thing but makes the demo simpler.
-
 from opsmop.core.easy import *
 import getpass
 USERNAME = getpass.getuser()
@@ -13,17 +9,6 @@ class FeatureOne(Role):
 
     def should_process_when(self):
         return UserFacts.get('feature_flags','ff01')
-
-    # This resource, as defined in the policy is gated by a feature flag.
-    # the feature flag lives in /etc/opsmop/facts.d/feature_flags.json
-    #
-    # In the stock example, this feature flag is ENABLED
-    #
-    # Additionally, some bonus facts come from /etc/opsmop/facts.d/star_trek.yml
-    # and /etc/opsmop/facts.d/dynamic_facts.sh.  
-    #
-    # Facts can be JSON, YAML, or executable files returning JSON or YAML.
-
 
     def set_resources(self):
 
@@ -38,11 +23,6 @@ class FeatureOne(Role):
 
 class FeatureTwo(Role):
 
-    # This resource, as defined in the policy is gated by a feature flag.
-    # the feature flag lives in /etc/opsmop/facts.d/feature_flags.json
-    #
-    # In the stock example, this feature flag is DISABLED
-
     def should_process_when(self):
         return UserFacts.feature_flags.get('ff02',False)
 
@@ -52,16 +32,7 @@ class FeatureTwo(Role):
             DebugFacts()
         )
 
-
 class CommonSetup(Role):
-
-    # A BIT META - THIS IS NOT PART OF THE DEMO EXPLICITLY
-    #
-    # the above roles shows what happens when you add files to /etc/opsmop/facts.d
-    # this role actually SETS UP /etc/opsmop/facts.d 
-    #
-    # Because we are mucking with the facts we use a rarely used traversal hook
-    # in the API to clear it so other roles aren't impacted.
 
     def set_resources(self):
         resources = Resources()
@@ -81,7 +52,7 @@ class CommonSetup(Role):
             )
         )
 
-    def post(self, apply=False):
+    def post(self):
         UserFacts.invalidate()
 
 class Demo(Policy):
