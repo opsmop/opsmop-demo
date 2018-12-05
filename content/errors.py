5874837486bfe9be341f63640bbe37538c732894
Michael DeHaan <michael@michaeldehaan.net>
2018-12-05 09:18:05 -0500

    Add some more examples to errors.py demo

--- content/errors.py
+++ content/errors.py
@@ -1,4 +1,4 @@
-# a simple demo showing how to ignore the errors of a shell command
+# a simple demo showing how to ignore and manipulate the error status of the shell command
 
 from opsmop.core.easy import *
 import random 
@@ -6,15 +6,29 @@ import random
 class Main(Role):
 
     def set_variables(self):
-        return dict()
+        return dict(a=5, b=6, c=True)
 
     def set_resources(self):
 
+        MATCHED_OUTPUT = Eval('"hi" in z.data')
+
         return Resources(
+            
+            # a non-zero return code, so this will pass 
             Shell("/usr/bin/true"),
+            
+            # explicitly ignoring errors
             Shell("/usr/bin/false", ignore_errors=True, register='x'),
             Echo("the result was {{ x }} - {{ x.rc }} - {{ x.data }}"),
-            Shell("/usr/bin/false")
+            
+            # conditionally ignoring errors, in a trivial way
+            Shell("/usr/bin/false", failed_when=False),
+
+            # conditionally deciding when to ignore errors
+            Shell("/usr/bin/false", register='y', failed_when=Eval('y.rc !=0 and not c')),
+
+            # considering the output to decide when to fail
+            Shell("echo hi", register='z', failed_when=MATCHED_OUTPUT)
         )
 
 
