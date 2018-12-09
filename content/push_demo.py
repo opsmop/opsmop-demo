be7fa2319f4beedd73a49292e0883fde18c3e9ce
Michael DeHaan <michael@michaeldehaan.net>
2018-12-09 18:12:38 -0500

    Update demo repo

--- content/push_demo.py
+++ content/push_demo.py
@@ -10,17 +10,30 @@ from opsmop.core.easy import *
     
 inventory = TomlInventory("inventory/inventory.toml")
 
+class BaseRole(Role):
+
+    def ssh_as(self):
+        # most users will want to omit this method and instead have a ~/.opsmop/defaults.toml
+        return ('mpdehaan', None)
+
+    def sudo(self):
+        return True
+
+    def sudo_as(self):
+        # if required, a password can be read from ~/.opsmop/defaults.toml
+        return ('root', None)
+
 class WebServers(Role):
 
-    def push_to(self):
+    def inventory(self):
         return inventory.filter('webservers*')
 
     def set_variables(self):
         return dict(what='bar', code=1234)
 
     def set_resources(self):
-
         return Resources(
+            Debug(),
             Shell("uname -a")
         )
 
