b17c895e5a0e1beca1e1a2d890360f30abd58c87
Michael DeHaan <michael@michaeldehaan.net>
2019-01-17 15:14:56 -0500

    adapt the error handling example

--- content/errors.py
+++ content/errors.py
@@ -7,28 +7,23 @@ class Main(Role):
     def set_variables(self):
         return dict(a=5, b=6, c=True)
 
-    def set_resources(self):
+    def main(self):
 
-        MATCHED_OUTPUT = Eval('"hi" in z.data')
-
-        return Resources(
+        # a non-zero return code, so this will pass 
+        Shell("/usr/bin/true"),
             
-            # a non-zero return code, so this will pass 
-            Shell("/usr/bin/true"),
-            
-            # explicitly ignoring errors
-            Shell("/usr/bin/false", ignore_errors=True, register='x'),
-            Echo("the result was {{ x }} - {{ x.rc }} - {{ x.data }}"),
-            
-            # conditionally ignoring errors, in a trivial way
-            Shell("/usr/bin/false", failed_when=False),
+        # explicitly ignoring errors
+        x = Shell("/usr/bin/false", ignore_errors=True)
+        Echo("the result was {{ x }} - {{ x.rc }} - {{ x.data }}")
+        
+        # conditionally ignoring errors, in a trivial way
+        Shell("/usr/bin/false", failed_when=False)
 
-            # conditionally deciding when to ignore errors
-            Shell("/usr/bin/false", register='y', failed_when=Eval('y.rc !=0 and not c')),
+        # conditionally deciding when to ignore errors
+        Shell("/usr/bin/false", failed_when=lambda o: o.rc != 0)
 
-            # considering the output to decide when to fail, also showing the condition assigned to a variable
-            Shell("echo hi", register='z', failed_when=MATCHED_OUTPUT)
-        )
+        # considering the output to decide when to fail, also showing the condition assigned to a variable
+        Shell("echo hi", failed_when=lambda o: 'hi' in o.data)
 
 
 class Demo(Policy):
